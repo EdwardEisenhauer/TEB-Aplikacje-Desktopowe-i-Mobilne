5464b158aa7622d5f3399184d9ca8f98111d6b2b
EdwardEisenhauer <sergiusz.warga@gmail.com>
2020-10-21 15:39:56 +0200

    Added main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+image = {   "type": "P1",
+            "comment": "komentarz",
+            "size": [2, 3],
+            "pixels": [ [1, 0], 
+                        [0, 1],
+                        [0, 1]]
+        }
+
+
+def load_image(filename):
+    return image
+
+
+def save_image(filename, image):
+    pass
+
