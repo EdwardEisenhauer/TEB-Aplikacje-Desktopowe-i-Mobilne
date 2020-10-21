d7a147507f00b26aad0fca7bb7947cfe6d16e3c5
EdwardEisenhauer <sergiusz.warga@gmail.com>
2020-10-21 16:31:20 +0200

    Added bryk.py

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-image = {   "type": "P1",
-            "comment": "komentarz",
-            "size": [2, 3],
-            "pixels": [ [1, 0], 
-                        [0, 1],
-                        [0, 1]]
-        }
 
 
 def load_image(filename):
+    image = {   "type": "P1",
+                "comment": "komentarz",
+                "size": [2, 3],
+                "pixels": [ [1, 0], 
+                            [0, 1],
+                            [0, 1]]
+            }
     return image
 
 
 def save_image(filename, image):
     pass
 
+image = load_image("nazwa_pliku")
+save_image("inna_nazwa", image)
