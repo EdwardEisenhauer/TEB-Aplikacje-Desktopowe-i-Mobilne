d1b9d318a2039ae780ba5e1e99fc7022274b68b6
EdwardEisenhauer <sergiusz.warga@gmail.com>
2020-10-21 16:33:33 +0200

    Added comments to main.py

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-
-
 def load_image(filename):
     image = {   "type": "P1",
                 "comment": "komentarz",
@@ -8,10 +6,16 @@ def load_image(filename):
                             [0, 1],
                             [0, 1]]
             }
+    # Otworzyć plik
+    # Wczytać nagłówki do odpowiednich stron słownika
+    # Na podstawie rozmiaru wczytać piksele
     return image
 
 
 def save_image(filename, image):
+    # Otowrzyć plik
+    # Zapisać nagłówki ze słownika do pliku
+    # Na podstawie rozmiaru zapisać piksele
     pass
 
 image = load_image("nazwa_pliku")
