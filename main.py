21995ee832b5c6e6fd0c7c9cb011446ae55393d6
sergiusz <sergiusz.warga@gmail.com>
2020-11-19 10:35:42 +0100

    Updated load_image() and added negative()

--- main.py
+++ main.py
@@ -1,26 +1,52 @@
 def load_image(filename):
+    image = {}
+    image["type"] = ''
+    image["size"] = []
+    image["comment"] = ''
+    image["pixels"] = []
+
     infile = open(filename, "r")
     instr = infile.read().split('\n')
     image["type"] = instr.pop(0)
-    if image["comment"]:
+
+    if instr[0][0] == '#':
         image["comment"] = instr.pop(0)
+
     image["size"] = list(map(int, instr.pop(0).split(' ')))
+
     for row in instr:
         image["pixels"].append(list(map(int, row.split(' '))))
     return image
 
 
-def generate_square(size, colour=True):
+def generate_square(size, colour):
     image = {}
     image["type"] = "P1"
     image["size"] = [size, size]
     image["comment"] = ''
     image["pixels"] = []
+
     for row in range(size):
         line = []
         for col in range(size):
-            line.append(1)
+            if colour:
+                line.append(0)
+            else:
+                line.append(1)
         image["pixels"].append(line)
+
+    return image
+
+
+def negative(image):
+    for row in range(image["size"][1]):
+        # print(image["pixels"][row])
+        for col in range(image["size"][0]):
+            if image["pixels"][row][col] == 0:
+                image["pixels"][row][col] = 1
+            else:
+                image["pixels"][row][col] = 0
+
     return image
 
 
@@ -49,6 +75,14 @@ def save_image(filename, image):
     for row in image["pixels"]:
         outfile.write(' '.join(list(map(str, row))) + '\n') # Change ints to strs and join them with spaces
 
-image = generate_square(3, True)
-# image = load_image("test.pgm")
-save_image("black.pgm", image)
+# image = generate_square(5, True)
+# # image = load_image("test.pgm")
+# save_image("white.pgm", image)
+# image = generate_square(5, False)
+# # image = load_image("test.pgm")
+# save_image("black.pgm", image)
+
+image = load_image("arrow.pgm")
+image = negative(image)
+save_image("inverted_arrow.pgm", image)
+
